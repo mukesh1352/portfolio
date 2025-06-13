e27d0aa53f105ed79f5a6206ce3c09fe245c345e
mukesh <140415041+mukesh1352@users.noreply.github.com>
2025-06-13 07:45:50 +0530

    removed the hero section

--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -1,10 +1,8 @@
 import Header from "../components/header"
-import Hero from "../components/hero"
 export default function Main(){
     return(
         <div>
             <Header/>
-            <Hero/>
         </div>
     )
 }
